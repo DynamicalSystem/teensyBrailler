c92d5e62e75200d38f5e31675540f2149a75908c
Simon Horrobin <simon@horrobin.net>
2014-12-20 11:46:59 +0000

    First draft of code
    Uses Bounce to detect 10 key presses, up and down.

---
+++ teensyBrailler.ino
@@ -0,0 +1,26 @@
+#include <Bounce2.h>
+#define NUM_BUTTONS 10
+#define NUM_FINGERS 5
+
+#define LEFT_HANDED TRUE
+
+#define UP LOW          //Swap these if the buttons are pull down
+#define DOWN HIGH
+
+Bounce brailleCells[NUM_BUTTONS];
+
+void setup() {
+  //We assume Pin 6 (teensy++ LED) is available via a 1K pullup
+  for (i = 0; i < NUM_BUTTONS; i = i + 1) {
+    brailleCells[i] = Bounce();
+    brailleCells[i].attach(i);
+    brailCells[i].interval(5);
+    pinMode(i, INPUT_PULLUP);
+  }
+}
+
+void loop() {
+  for (i = 0; i < NUM_BUTTONS; i = i + 1) {
+    brailleCells[i].update();
+  }
+}
